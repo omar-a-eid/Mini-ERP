bbe34196e47e84e9af6f6baca43e62de06064658
omar-a-eid <omerx9988@gmail.com>
2024-10-19 19:40:52 +0300

    Making childern width 100%

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
+import Header from "../components/header/Header";
 import SideBar from "../components/navigation/SideBar";
 export default function MainLayout() {
   return (
     <div style={{display: "flex"}}>
       <SideBar />
 
-      <div>
+      <div style={{flex: 1}}>
+        <Header />
         <div></div>
       </div>
 
